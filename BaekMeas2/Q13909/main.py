372c79c778dc25968e86325f48cd6c07db28c50f
ChaiTope <gksmdk5094@gmail.com>
2025-03-09 15:44:53 +0900

    약수 소수 배수 2 문제풀이 끝

--- BaekMeas2/Q13909/main.py
+++ BaekMeas2/Q13909/main.py
@@ -1,15 +1,6 @@
+from math import floor
 n = int(input())
 
-arr = [1] * n
-for i in range(n):
-    j = i
-    p = 1
-    while j*p < n:
-        if arr[j*p] == 0:
-            arr[j] = 1
-            j += 1
-        else:
-            arr[j*p] = 0
-            j += 1
-    p+=1
-    print(*arr)
+count = floor(n**0.5)
+
+print(count)
