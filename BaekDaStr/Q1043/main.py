69df94b6c33b88a88ec90b6819bade452ba3ed59
ChaiTope <gksmdk5094@gmail.com>
2025-06-18 16:03:18 +0900

    풀이 완료

--- BaekDaStr/Q1043/main.py
+++ BaekDaStr/Q1043/main.py
@@ -1,22 +1,33 @@
 import sys
+from itertools import combinations
 
 input = sys.stdin.readline
 
 N, M = map(int, input().split())
-
 truth = list(map(int, input().split()))
 
-if len(truth) == 1:
-    print(M)
-else:
-    peoples = [0] * N
+# 진실을 아는 사람 집합
+peoples = [False]*(N+1)
+for p in truth[1:]:
+    peoples[p] = True
 
-    parties = []
-    for i in range(M):
-        party = list(map(int, input().split()))
-        parties.append(party)
+# 파티별 참석자만 담기
+parties = []
+for _ in range(M):
+    data = list(map(int, input().split()))
+    parties.append(data[1:])   # data[0]은 인원수
 
-        for people in party:
-            if people in truth:
-                #일단 여기까지
-                peoples.remove(people)
+# 반복 스캔으로 진실 전파
+changed = True
+while changed:
+    changed = False
+    for party in parties:
+        if any(peoples[p] for p in party):
+            for p in party:
+                if not peoples[p]:
+                    peoples[p] = True
+                    changed = True
+
+# 거짓말 가능한 파티 개수
+ans = sum(1 for party in parties if not any(peoples[p] for p in party))
+print(ans)
