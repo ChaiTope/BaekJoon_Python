d5045ceaea169071fd3b0042c3cb4073f3388500
ChaiTope <gksmdk5094@gmail.com>
2025-10-10 17:23:21 +0900

    트리의 지름 문제 풀이

--- BaekGraph/Q1167/main.py
+++ BaekGraph/Q1167/main.py
@@ -1,9 +1,10 @@
 import sys
+import heapq
 
 input = sys.stdin.readline
 
 V = int(input())
-tree = [0 for _ in range(V + 1)]
+tree = [[] for _ in range(V + 1)]
 
 for i in range(V):
     nodes = list(map(int, input().split()))
@@ -13,4 +14,36 @@ for i in range(V):
         if nodes[j] == -1:
             break
 
-        tree[node].append()
+        tree[node].append([nodes[j], nodes[j+1]])
+
+def dijkstra(start, adj):
+    INF = float('inf')
+    n = len(adj)               # 정점 수
+    dist = [INF] * n
+    dist[start] = 0
+
+    pq = []
+    heapq.heappush(pq, (0, start))  # (거리, 정점)
+    loc = 0
+    max_val = 0
+    while pq:
+        d, u = heapq.heappop(pq)
+        if d > dist[u]:
+            continue
+
+        # 여기서 '확정'이 됐으니 이때 갱신
+        loc = u
+        max_val = d
+
+        for v, w in adj[u]:
+            nd = d + w
+            if nd < dist[v]:
+                dist[v] = nd
+                heapq.heappush(pq, (nd, v))
+
+    return loc, max_val
+
+ch, ch_res = dijkstra(1, tree)
+adj, res = dijkstra(ch, tree)
+
+print(res)
